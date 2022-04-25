49c485544a0e72b979f59540fd4ce578e4cf210f
PorkSkulls <gabrielvidal1372@gmail.com>
2022-04-24 22:01:09 -0400

    Created POM

---
+++ src/main/java/org/cloverservices/invisregions/CloverInvisRegions.java
@@ -0,0 +1,17 @@
+package org.cloverservices.invisregions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CloverInvisRegions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
